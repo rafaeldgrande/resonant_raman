83e4b7f7e6b9fb8da6b405ad2b93ab70a04dce82
Rafael Del Grande <rafaeldgrande@gmail.com>
2026-03-19 19:58:32 -0600

    fixed indexes bug

--- assemble_exciton_phonon_coeffs.py
+++ assemble_exciton_phonon_coeffs.py
@@ -33,32 +33,33 @@ rpa_diag, rpa_offdiag, rpa_diag_plus_kernel = read_excited_state_forces_file_ph(
 Nmodes = rpa_diag.shape[0]
 print(f'Number of phonon modes detected: {Nmodes}')
 
-data = np.zeros((3, Nmodes, max_index+1, max_index+1), dtype=complex)
+data = np.zeros((3, Nmodes, max_index, max_index), dtype=complex)
 
 print('Loading exciton-phonon coupling data from files forces_ph.out_i_j')
 # loading all data
 
 counter = 0
 for pair in pair_indexes_to_load:
-    i_exciton_1 = pair[0]
+    i_exciton_1 = pair[0] # starts at 1
     i_exciton_2 = pair[1]
     
     filename = f'forces_ph.out_{i_exciton_1}_{i_exciton_2}'
     rpa_diag, rpa_offdiag, rpa_diag_plus_kernel = read_excited_state_forces_file_ph(filename)
     
-    data[0, :, i_exciton_1, i_exciton_2] = rpa_diag
-    data[1, :, i_exciton_2, i_exciton_1] = rpa_offdiag
-    data[2, :, i_exciton_1, i_exciton_2] = rpa_diag_plus_kernel
+    data[0, :, i_exciton_1-1, i_exciton_2-1] = rpa_diag
+    data[1, :, i_exciton_1-1, i_exciton_2-1] = rpa_offdiag
+    data[2, :, i_exciton_1-1, i_exciton_2-1] = rpa_diag_plus_kernel
     
     # assuming there is only one i, j and not j, i
     if i_exciton_1 != i_exciton_2:
-        data[0, :, i_exciton_2, i_exciton_1] = rpa_diag.conjugate()
-        data[1, :, i_exciton_1, i_exciton_2] = rpa_offdiag.conjugate()
-        data[2, :, i_exciton_2, i_exciton_1] = rpa_diag_plus_kernel.conjugate()
+        data[0, :, i_exciton_2-1, i_exciton_1-1] = rpa_diag.conjugate()
+        data[1, :, i_exciton_2-1, i_exciton_1-1] = rpa_offdiag.conjugate()
+        data[2, :, i_exciton_2-1, i_exciton_1-1] = rpa_diag_plus_kernel.conjugate()
     counter += 1
     
     if counter % 100 == 0:
         print(f'  Loaded {counter} pairs of {len(pair_indexes_to_load)} ({100.0*counter/len(pair_indexes_to_load):.2f} %)')
+
 # saving data in hdf5 file
 with h5py.File('exciton_phonon_couplings.h5', 'w') as hf:
     hf.create_dataset('rpa_diag', data=data[0])
